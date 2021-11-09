073f51df35d7a2ddef2466c968e454d123e83dfd
Kumar <mohitnevatru@gmail.com>
2021-11-09 21:33:32 +0530

    react, bootstrap router, Product and Homescreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,28 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to TechCube!</h1>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
 export default App
+{
+  /* //exact to sure only / routes to this component and not any other
+          routes like /login /abc etc. */
+}
