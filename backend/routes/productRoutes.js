3e798dec3ac3bff500cecfd3dc6ae2258293f9ec
Kumar <mohitnevatru@gmail.com>
2021-11-12 21:59:30 +0530

    product controller and routes created

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,10 @@
 import express from 'express'
-import Product from '../models/productModel.js'
-import asyncHandler from 'express-async-handler' //to handle error in our express routes with one single error handler
+import { getProducts, getProductById } from '../controller/productController.js'
 
 const router = express.Router()
 
-// @desc    Fetch all products
-// @route   GET/api/products
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    // res.json() or res.send()
-    // auto converts JS object to JSON
-    res.json(products)
-  })
-)
+router.route('/').get(getProducts)
 
-// @desc    Fetch a single product
-// @route   GET/api/products/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 export default router
