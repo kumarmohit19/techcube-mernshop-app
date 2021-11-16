09256298afa84f5b204dcc4c7247a3edb5269534
Kumar <mohitnevatru@gmail.com>
2021-11-16 16:44:28 +0530

    get user order module completed

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,7 @@ import {
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
+  orderListMyReducer,
 } from './reducers/orderReducers'
 import {
   userLoginReducer,
@@ -30,6 +31,7 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
