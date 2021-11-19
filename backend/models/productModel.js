4d9ca0d90c1e2e02e3ab88a3f6ec88a621f4a955
Kumar <mohitnevatru@gmail.com>
2021-11-19 16:20:04 +0530

    backend funtionality for product review

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,6 +14,11 @@ const reviewSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId, // links to a mongoDB schema id
+      required: true,
+      ref: 'User', // reference to the model
+    },
   },
   {
     timestamps: true,
